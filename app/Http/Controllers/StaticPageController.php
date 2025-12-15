a4a59e0140ec18ee925145c0d17a7146c81e7bd5
leo_regniez1 <leo_regniez1@ens.univ-artois.fr>
2025-12-15 10:52:18 +0100

    ajouter routes et contrôleurs initiaux (issues #14)

---
+++ app/Http/Controllers/StaticPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class StaticPageController extends Controller
+{
+    public function about(): View
+    {
+        return view('pages.about');
+    }
+
+    public function contact(): View
+    {
+        return view('pages.contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StaticPageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/a-propos', [StaticPageController::class, 'about'])->name('about');
+Route::get('/contact', [StaticPageController::class, 'contact'])->name('contact');
